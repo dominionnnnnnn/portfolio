81887c157832fdbd98288bef11959dbf59cfd8dc
dominion <adebiyiquawiy2006@gmail.com>
2025-05-26 10:58:02 +0100

    404 not found

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="text-center p-8 mt-18">
-      <h1 className="text-4xl font-bold mb-4 ">404 - Page Not Found</h1>
-      <p className="mb-4">The page you're looking for doesn't exist.</p>
-      <Link to="/" className="text-blue-500 underline">Go Home</Link>
+    <div className="text-center h-screen bg-center bg-cover p-8 flex justify-center items-center flex-col " style={{ backgroundImage: "url('/notfound.png')"}}>
+      <h1 className="text-8xl font-extrabold mb-4 text-white name ">404</h1>
+      <p className="mb-4 text-2xl font-bold text-[#00A8E8]">The page you're looking for doesn't exist.</p>
+      <Link to="/" className="text-black py-1 px-3 font-bold rounded-xl text-lg bg-[#00A8E8]">GO HOME!</Link>
     </div>
   );
 };
