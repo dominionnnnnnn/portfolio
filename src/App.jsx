b28cfb64e3c21a220cbbcf823b9089bab805e50a
dominion <adebiyiquawiy2006@gmail.com>
2025-05-31 00:52:01 +0100

    project page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Mainlayout from "./layouts/Mainlayout"
 import HomePage from "./pages/HomePage"
 import AboutPage from "./pages/AboutPage"
+import ProjectsPage from "./pages/ProjectsPage"
 import NotFound from "./components/NotFound"
+import ExpLine from "./components/ExpLine"
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
 
 const router = createBrowserRouter(
@@ -10,7 +12,9 @@ const router = createBrowserRouter(
       <Route path="/" element={<Mainlayout />} >
         <Route index element={<HomePage/>} />
         <Route path="/about" element={<AboutPage/>} />
+        <Route path="/projects" element={<ProjectsPage/>}/>
       </Route>
+        <Route path="/timeline" element={<ExpLine/>} />
         <Route path="*" element={<NotFound/>} />
     </>
   )
---
+++ src/components/ExpLine.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TimeCard from './TImeCard'
+
+const ExpLine = () => {
+  return (
+    <div className='relative'>
+      <TimeCard
+        title="Software Engineer"
+        time="Jan 2020 - Present"
+        content="Developing and maintaining web applications using React and Node.js."/>
+    </div>
+  )
+}
+
+export default ExpLine
