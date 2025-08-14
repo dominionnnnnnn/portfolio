a258057e11e871036b73fbd0b630199b395ec264
Dominion <adebiyiquawiy2006@gmail.com>
2025-08-14 11:35:51 +0100

    anaylytics

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -3,10 +3,12 @@ import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer";
 import ParticleBg from "../components/ParticleBg";
 import ScrollToTop from "../components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/next"
 
 const Mainlayout = () => {
   return (
     <>
+        <Analytics />
         <ScrollToTop/>
         <ParticleBg />
         <Navbar />
