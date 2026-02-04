f68bdd80a550c22901c3267e2e915bde1f1c8c78
Dominion <adebiyiquawiy2006@gmail.com>
2026-02-04 22:19:46 +0100

    push

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='border-t-1 border-gray-500 py-4 flex justify-between px-8'>
+      <h1 className='text-white font-extrabold name'>DOMDEV</h1>
+      <p className='text-white font-extrabold '>© All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
